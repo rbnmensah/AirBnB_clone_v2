2082d86bf87dc6c3347054a1239f24ab32d2fff9
Reuben <rbnmensah@gmail.com>
2023-12-07 01:53:23 +0000

    airbnb_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,33 +18,33 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if os.path.isfile(archive_path) is False:
+    if os.path.isfile(archive_path) == False:
         return False
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    if put(archive_path, "/tmp/{}".format(file)).failed == True:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+           format(name)).failed == True:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+           format(name)).failed == True:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+           format(file, name)).failed == True:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    if run("rm /tmp/{}".format(file)).failed == True:
         return False
     if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+           "/data/web_static/releases/{}/".format(name, name)).failed == True:
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+           format(name)).failed == True:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+    if run("rm -rf /data/web_static/current").failed == True:
         return False
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+           format(name)).failed == True:
         return False
     return True
