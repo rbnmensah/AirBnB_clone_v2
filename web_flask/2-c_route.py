853d53c9d817b5841a50309e7fd2390fd82e4af0
Reuben Mensah <rbnmensah@gmail.com>
2024-01-03 01:15:21 +0000

    newFile

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application C is FUN"""
+""" 2. Script to start a Flask web application with 3 view functions """
+
 from flask import Flask
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Prints a Message when / is called """
+@app.route('/')
+def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ Prints a Message when /hbnb is called """
+@app.route('/hbnb')
+def hello():
+    """ Return other text. """
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """ Prints a Message when /c is called """
-    return "C " + text.replace('_', ' ')
+@app.route('/c/<text>')
+def c_text(text):
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
-if __name__ == "__main__":
-    """ Main Function """
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+""" 7. Start flask service that does something. """
 
-from flask import Flask, render_template
-from models import *
-from models import storage
+from flask import Flask
+from flask import render_template
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+#@app.route()
 
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
-    storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
